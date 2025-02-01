274b5f5e216024101944e445b289e4a990f216f4
mohmdrezaei <mhwmdrz9@gmail.com>
2025-02-01 23:01:08 +0330

    Implementing a token-based profile API

--- app.js
+++ app.js
@@ -1,45 +1,53 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const mongoose = require('mongoose')
-const User =require("./models/User")
-const bcrypt = require('bcryptjs');
-const jwt =require("jsonwebtoken")
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
-const secret = "dfre58439uihrjgyui3y4ui"
+const secret = "dfre58439uihrjgyui3y4ui";
 const salt = bcrypt.genSaltSync(10);
-app.use(cors({credentials : true , origin :"http://localhost:3000"}));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
+app.use(cookieParser());
 
 const dbURI =
   "mongodb+srv://mohammad:12345@cluster0.rvsiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(dbURI);
 
-app.post("/register", async(req, res) => {
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
- const userDoc = await User.create({
-    username ,
-     password : bcrypt.hashSync(password, salt)
-})
+  const userDoc = await User.create({
+    username,
+    password: bcrypt.hashSync(password, salt),
+  });
   res.json(userDoc);
 });
 
-app.post("/login" , async(req , res)=>{
-  const {username , password } = req.body
-  const userDoc =await User.findOne({username})
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
   const passOk = bcrypt.compareSync(password, userDoc.password);
-  if(passOk){
-    jwt.sign({username ,  id:userDoc._id} , secret ,(err , token)=>{
-    if(err) throw err
-    res.cookie("token" , token).json("ok")
-   })
-  
-
-  }else{
-    res.status(400).json("wrong credentials")
+  if (passOk) {
+    jwt.sign({ username, id: userDoc._id }, secret, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("ok");
+    });
+  } else {
+    res.status(400).json("wrong credentials");
   }
-})
+});
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("welcome to blog API");
